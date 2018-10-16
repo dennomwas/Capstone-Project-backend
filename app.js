8be78a073e323b5bd59092ce8adf927d380b1c5b
dennomwas <githinji.mwangi@gmail.com>
2018-10-16 15:03:39 +0300

    [Ch] Add error handlers to Express server
    - Add and configure json body-parser

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 'use strict'
 
 const express = require('express');
+const jsonParser = require('body-parser').json;
 const app = express();
+
+// parse incoming requests to json 
+app.use(jsonParser());
+
+// Handle 404 errors
+app.use((req, res, next) => {
+    const err = new Error(" Not Found");
+    err.status = 404;
+    next(err);
+});
+
+// Error handler
+app.use((err, res, req, next) => {
+    res.status(err.status || 500);
+    res.json({
+        error: { 
+            message: err.message 
+        }
+    });
+});
 
 // define server port
 const port = process.env.PORT || 3000;
