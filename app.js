4237fd08d479170db501cb929566a35cb756810f
dennomwas <githinji.mwangi@gmail.com>
2018-11-14 19:28:02 +0300

    Fix Conflicts

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ const app = express();
 app.use(jsonParser());
 
 // Db connection using mongoose
-mongoose.connect('mongodb://localhost:27017/capstone-project', { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost:27017/capstone-project-db', {
+    useNewUrlParser: true
+});
 const db = mongoose.connection;
 
 db.on('error', (err) => {
@@ -27,11 +29,11 @@ app.use((req, res, next) => {
 });
 
 // Error handler
-app.use((err, res, req, next) => {
+app.use((err, req, res, next) => {
     res.status(err.status || 500);
     res.json({
-        error: { 
-            message: err.message 
+        error: {
+            message: err.message
         }
     });
 });
@@ -40,5 +42,5 @@ app.use((err, res, req, next) => {
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log('Express server is listening on localhost:',port)
+    console.log('Express server is listening on localhost:', port)
 });
--- models/index.js
+++ models/index.js
@@ -4,20 +4,29 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 
-const userSchema = new Schema({
-    fullNames: {
+const UserSchema = new Schema({
+    firstName: {
         type: String,
-        required: true,
+        required: [true, 'A first name is required'],
+        trim: true
+    },
+    lastName: {
+        type: String,
+        required: [true, 'A last name is required'],
         trim: true
     },
     email: {
         type: String,
         unique: true,
-        required: true,
+        required: [true, 'An Email is required'],
         trim: true
     },
     password: {
-        required: true,
+        required: [true, 'A Password is required'],
+        trim: true
+    },
+    passwordConfirmation: {
+        required: [true, 'Password confirmation is required'],
         trim: true
     }
 });
@@ -25,3 +34,34 @@ const userSchema = new Schema({
 const userSchema = mongoose.model('forecastData', userSchema);
 
 module.exports.userSchema = userSchema;
+
+// check if user exists
+UserSchema.statics.authenticate = (email, password, callback) => {
+    User.findOne({
+        email: email
+    }).exec((error, user) => {
+        if (error) {
+            return callback(error)
+        } else if (!user) {
+            const err = new Error('User not found!')
+            err.status = 401;
+            return callback(err);
+        };
+
+    });
+};
+
+// hash password before saving
+UserSchema.pre('save', function (next) {
+    const user = this;
+    bcrypt.hash(user.password, 10, function (err, hash) {
+        if (err) {
+            return next(err);
+        }
+        user.password = hash;
+        next();
+    })
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
